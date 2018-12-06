0e840610317c153022307d382d521e8a372739c7
Yusuke Matsunaga <yusuke_matsunaga@ieee.org>
2018-12-06 16:44:11 +0900

    Sop.h: 変更
    * SopPat のストリーム出力を追加
    * SopPat を nsYm 直下に移動

--- include/ym/Sop.h
+++ include/ym/Sop.h
@@ -14,13 +14,6 @@
 
 BEGIN_NAMESPACE_YM_LOGIC
 
-/// @brief パタンを表す列挙型
-enum class SopPat {
-  _X = 0, //@< なし
-  _1 = 1, //@< 正極性
-  _0 = 2  //@< 負極性
-};
-
 /// @brief SopPat をパックしたビットベクタ型
 using SopBitVect = ymuint64;
 
@@ -29,5 +22,34 @@ class SopBlock;
 class SopMgr;
 
 END_NAMESPACE_YM_LOGIC
+
+BEGIN_NAMESPACE_YM
+
+/// @brief パタンを表す列挙型
+enum class SopPat {
+  _X = 0, //@< なし
+  _1 = 1, //@< 正極性
+  _0 = 2  //@< 負極性
+};
+
+/// @relates SopPat
+/// @brief SopPat のストリーム出力
+/// @param[in] s ストリーム
+/// @param[in] pat パタン
+inline
+ostream&
+operator<<(ostream& s,
+	   SopPat pat)
+{
+  switch ( pat ) {
+  case SopPat::_0: s << '0'; break;
+  case SopPat::_1: s << '1'; break;
+  case SopPat::_X: s << 'X'; break;
+  default: ASSERT_NOT_REACHED;
+  }
+  return s;
+}
+
+END_NAMESPACE_YM
 
 #endif // SOP_H
