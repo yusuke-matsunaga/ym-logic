b6111225a32ef302dd31ac8df41a14d80e5dc4c5
Yusuke Matsunaga <yusuke_matsunaga@ieee.org>
2022-07-13 20:15:01 +0900

    TvFunc::BCF() 作成

---
+++ c++-srcs/tvfunc/TvFunc_bcf.cc
@@ -0,0 +1,139 @@
+
+/// @file TvFunc_bcf.cc
+/// @brief TvFunc_bcf の実装ファイル
+/// @author Yusuke Matsunaga (松永 裕介)
+///
+/// Copyright (C) 2022 Yusuke Matsunaga
+/// All rights reserved.
+
+#include "ym/TvFunc.h"
+#include "ym/SopCover.h"
+#include "ym/SopCube.h"
+
+
+BEGIN_NAMESPACE_YM
+
+BEGIN_NONAMESPACE
+
+bool debug = false;
+
+vector<SopCube>
+bcf_sub(
+  SizeType ni,
+  const TvFunc& f,
+  SizeType var
+)
+{
+  if ( debug ) {
+    cout << "bcf_sub: " << f << ": " << var << endl;
+  }
+
+  if ( f.is_zero() ) {
+    if ( debug ) {
+      cout << " ==> 0" << endl;
+    }
+    return vector<SopCube>{};
+  }
+  if ( f.is_one() ) {
+    if ( debug ) {
+      cout << " ==> 1" << endl;
+    }
+    return vector<SopCube>{SopCube{ni}};
+  }
+
+  ASSERT_COND( var < ni );
+
+  VarId varid{var};
+
+  // コファクターに対して再帰する．
+  // ただし，共通部分を求めて3分割する．
+  auto f0 = f.cofactor(varid, true);
+  auto f1 = f.cofactor(varid, false);
+  auto fc = f0 & f1;
+  if ( debug ) {
+    cout << "  fc: " << fc << endl;
+    cout << "  f0: " << f0 << endl;
+    cout << "  f1: " << f1 << endl;
+  }
+  auto cc = bcf_sub(ni, fc, var + 1);
+  auto c0 = bcf_sub(ni, f0, var + 1);
+  auto c1 = bcf_sub(ni, f1, var + 1);
+  if ( debug ) {
+    cout << "  cc: ";
+    for ( auto& cube: cc ) {
+      cout << " " << cube;
+    }
+    cout << endl;
+    cout << "  c0: ";
+    for ( auto& cube: c0 ) {
+      cout << " " << cube;
+    }
+    cout << endl;
+    cout << "  c1: ";
+    for ( auto& cube: c1 ) {
+      cout << " " << cube;
+    }
+    cout << endl;
+  }
+  // 結果をマージする．
+  auto R = ~fc;
+  Literal lit0{varid, true};
+  for ( auto& cube: c0 ) {
+    TvFunc f_cube = TvFunc::make_one(ni);
+    for ( SizeType i = 0; i < ni; ++ i ) {
+      VarId var{i};
+      auto pat = cube.get_pat(var);
+      if ( pat == SopPat::_1 ) {
+	f_cube &= TvFunc::make_posi_literal(ni, var);
+      }
+      else if ( pat == SopPat::_0 ) {
+	f_cube &= TvFunc::make_nega_literal(ni, var);
+      }
+    }
+    if ( f_cube && R ) {
+      cc.push_back(cube * lit0);
+    }
+  }
+  Literal lit1{varid, false};
+  for ( auto& cube: c1 ) {
+    TvFunc f_cube = TvFunc::make_one(ni);
+    for ( SizeType i = 0; i < ni; ++ i ) {
+      VarId var{i};
+      auto pat = cube.get_pat(var);
+      if ( pat == SopPat::_1 ) {
+	f_cube &= TvFunc::make_posi_literal(ni, var);
+      }
+      else if ( pat == SopPat::_0 ) {
+	f_cube &= TvFunc::make_nega_literal(ni, var);
+      }
+    }
+    if ( f_cube && R ) {
+      cc.push_back(cube * lit1);
+    }
+  }
+
+  if ( debug ) {
+    cout << "bcf_sub: " << f << ": " << var << endl;
+    cout << " ==> ";
+    for ( auto& cube: cc ) {
+      cout << " " << cube;
+    }
+    cout << endl;
+  }
+
+  return cc;
+}
+
+END_NONAMESPACE
+
+// @brief Blake's Cannonical Form を求める．
+SopCover
+TvFunc::BCF() const
+{
+  SizeType ni = input_num();
+  auto cov = bcf_sub(ni, *this, 0);
+  sort(cov.begin(), cov.end());
+  return SopCover{ni, cov};
+}
+
+END_NAMESPACE_YM
--- tests/gtest/tvfunc/TvFuncTest.cc
+++ tests/gtest/tvfunc/TvFuncTest.cc
@@ -11,7 +11,8 @@
 #include "ym/NpnMap.h"
 #include "ym/RandPermGen.h"
 #include "ym/Range.h"
-#include <random>
+#include "ym/SopCube.h"
+//#include <random>
 
 
 BEGIN_NAMESPACE_YM
@@ -1110,6 +1111,183 @@ TEST(TvFuncTest, from_vect)
   TvFunc f{2, vect};
 
   EXPECT_EQ( "1110", f.str() );
+}
+
+SopCover
+prime_cover(
+  const TvFunc& func
+)
+{
+  SizeType ni = func.input_num();
+  SizeType ni_exp = 1 << ni;
+  // オンセットのリストを作る．
+  vector<SopCube> cube_list;
+  for ( SizeType p = 0; p < ni_exp; ++ p ) {
+    if ( func.value(p) ) {
+      vector<Literal> lit_list;
+      for ( SizeType i = 0; i < ni; ++ i ) {
+	Literal lit{VarId{i}, false};
+	if ( p & (1 << i) ) {
+	  lit_list.push_back(lit);
+	}
+	else {
+	  lit_list.push_back(~lit);
+	}
+      }
+      cube_list.push_back(SopCube{ni, lit_list});
+    }
+  }
+  vector<vector<SopCube>> cube_list_list(ni + 1);
+  cube_list_list[0] = cube_list;
+  unordered_set<SopCube> cube_mark;
+  unordered_set<SopCube> cube_check;
+  for ( SizeType d = 0; d < ni; ++ d ) {
+    const auto& cube_list = cube_list_list[d];
+    // 1ビット違いのキューブをマージする．
+    SizeType nc = cube_list.size();
+    for ( SizeType i0 = 0; i0 < nc; ++ i0 ) {
+      const auto& cube0 = cube_list[i0];
+      for ( SizeType i1 = i0 + 1; i1 < nc; ++ i1 ) {
+	const auto& cube1 = cube_list[i1];
+	SizeType dpos = -1;
+	vector<Literal> lit_list;
+	for ( SizeType i = 0; i < ni; ++ i ) {
+	  VarId var{i};
+	  auto pat0 = cube0.get_pat(var);
+	  auto pat1 = cube1.get_pat(var);
+	  if ( pat0 == pat1 ) {
+	    if ( pat0 == SopPat::_1 ) {
+	      lit_list.push_back(Literal{var, false});
+	    }
+	    else if ( pat0 == SopPat::_0 ) {
+	      lit_list.push_back(Literal{var, true});
+	    }
+	    continue;
+	  }
+	  if ( pat0 == SopPat::_X || pat1 == SopPat::_X ) {
+	    dpos = -1;
+	    break;
+	  }
+	  if ( dpos != -1 ) {
+	    dpos = -1;
+	    break;
+	  }
+	  dpos = i;
+	}
+	if ( dpos != -1 ) {
+	  SopCube new_cube{ni, lit_list};
+	  if ( cube_check.count(new_cube) == 0 ) {
+	    cube_list_list[d + 1].push_back(new_cube);
+	    cube_check.emplace(new_cube);
+	  }
+	  cube_mark.emplace(cube0);
+	  cube_mark.emplace(cube1);
+	}
+      }
+    }
+    if ( cube_list_list[d + 1].empty() ) {
+      break;
+    }
+  }
+  vector<SopCube> ans_list;
+  for ( const auto& cube_list: cube_list_list ) {
+    for ( const auto& cube: cube_list ) {
+      if ( cube_mark.count(cube) == 0 ) {
+	ans_list.push_back(cube);
+      }
+    }
+  }
+  sort(ans_list.begin(), ans_list.end());
+  return SopCover{ni, ans_list};
+}
+
+TEST(TvFuncTest, bcf1)
+{
+  vector<int> vect{0, 1, 1, 1};
+  TvFunc f{2, vect};
+
+  auto cov = f.BCF();
+
+  auto exp_cov = prime_cover(f);
+  EXPECT_EQ( exp_cov, cov );
+
+  ostringstream buf;
+  cov.print(buf);
+  EXPECT_EQ( "v1 + v0", buf.str() );
+}
+
+TEST(TvFuncTest, bcf2)
+{
+  vector<int> vect{0, 0, 0, 1, 0, 1, 1, 1};
+  TvFunc f{3, vect};
+
+  auto cov = f.BCF();
+
+  auto exp_cov = prime_cover(f);
+  EXPECT_EQ( exp_cov, cov );
+
+  ostringstream buf;
+  cov.print(buf);
+  EXPECT_EQ( "v1 v2 + v0 v2 + v0 v1", buf.str() );
+}
+
+TEST(TvFuncTest, bcf3)
+{
+  vector<int> vect{1, 1, 1, 1, 0, 1, 0, 0};
+  TvFunc f{3, vect};
+
+  auto cov = f.BCF();
+
+  auto exp_cov = prime_cover(f);
+  EXPECT_EQ( exp_cov, cov );
+}
+
+TEST_P(TvFuncTestWithParam, bcf)
+{
+  SizeType ni = GetParam();
+  SizeType ni_exp = 1 << ni;
+  SizeType n;
+  if ( ni < 1 ) {
+    return;
+  }
+  else if ( ni <= 4 ) {
+    n = (1 << ni_exp);
+  }
+  else if ( ni < 7 ) {
+    n = 1000;
+  }
+  else if ( ni < 10 ) {
+    n =100;
+  }
+  else {
+    return;
+  }
+  vector<int> values(ni_exp, 0);
+  for ( SizeType c: Range(n) ) {
+    for ( int p: Range(ni_exp) ) {
+      if ( ni < 4 ) {
+	if ( c & (1 << p) ) {
+	  values[p] = 1;
+	}
+	else {
+	  values[p] = 0;
+	}
+      }
+      else {
+	if ( mRandDist(mRandGen) ) {
+	  values[p] = 1;
+	}
+	else {
+	  values[p] = 0;
+	}
+      }
+    }
+    TvFunc func{ni, values};
+    auto cov = func.BCF();
+    // 正解を計算する．
+    auto exp_cov = prime_cover(func);
+    EXPECT_EQ( exp_cov, cov );
+  }
 }
 
 END_NAMESPACE_YM
