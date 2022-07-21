8d019044030311f16121a3d35088efce01e41d07
Yusuke Matsunaga <yusuke_matsunaga@ieee.org>
2022-07-22 00:34:41 +0900

    LuaBdd.cc: 作成中

--- c++-srcs/lua/LuaBdd.cc
+++ c++-srcs/lua/LuaBdd.cc
@@ -242,7 +242,7 @@ bddmgr_from_truth(
 
   SizeType n = lua.get_top();
   if ( n != 2 ) {
-    return lua.error_end("Error: BddMgr:from_truth() expects one arguments");
+    return lua.error_end("Error: BddMgr:from_truth() expects one argument.");
   }
 
   string str = lua.to_string(2);
@@ -252,6 +252,622 @@ bddmgr_from_truth(
 
   auto bddp = bdd_new(lua);
   *bddp = obj->from_truth(str);
+
+  return 1;
+}
+
+// ノード数を返す．
+int
+bddmgr_node_num(
+  lua_State* L
+)
+{
+  Luapp lua{L};
+
+  SizeType n = lua.get_top();
+  if ( n != 1 ) {
+    return lua.error_end("Error: BddMgr:node_num() expects no arguments.");
+  }
+
+  auto obj = LuaBdd::to_bddmgr(L, 1);
+  ASSERT_COND( obj != nullptr );
+
+  SizeType v = obj->node_num();
+
+  lua.push_integer(v);
+  return 1;
+}
+
+// GC を起動するしきい値を返す．
+int
+bddmgr_gc_limit(
+  lua_State* L
+)
+{
+  Luapp lua{L};
+
+  SizeType n = lua.get_top();
+  if ( n != 1 ) {
+    return lua.error_end("Error: BddMgr:gc_limit() expects no arguments.");
+  }
+
+  auto obj = LuaBdd::to_bddmgr(L, 1);
+  ASSERT_COND( obj != nullptr );
+
+  SizeType v = obj->gc_limit();
+
+  lua.push_integer(v);
+  return 1;
+}
+
+// GC を起動するしきい値を設定する．
+int
+bddmgr_set_gc_limit(
+  lua_State* L
+)
+{
+  Luapp lua{L};
+
+  SizeType n = lua.get_top();
+  if ( n != 2 ) {
+    return lua.error_end("Error: BddMgr:gc_limit() expects one argument.");
+  }
+
+  auto obj = LuaBdd::to_bddmgr(L, 1);
+  ASSERT_COND( obj != nullptr );
+
+  bool ok;
+  lua_Integer v;
+  tie(ok, v) = lua.to_integer(2);
+  if ( !ok ) {
+    return lua.error_end("Error: BddMgr::set_gc_limit(): 1st argument should be an integer.");
+  }
+
+  obj->set_gc_limit(v);
+
+  lua.push_boolean(true);
+  return 1;
+}
+
+// GC の起動を許可する．
+int
+bddmgr_enable_gc(
+  lua_State* L
+)
+{
+  Luapp lua{L};
+
+  SizeType n = lua.get_top();
+  if ( n != 1 ) {
+    return lua.error_end("Error: BddMgr:enable_gc() expects no arguments.");
+  }
+
+  auto obj = LuaBdd::to_bddmgr(L, 1);
+  ASSERT_COND( obj != nullptr );
+
+  obj->enable_gc();
+
+  lua.push_boolean(true);
+  return 1;
+}
+
+// GC の起動を禁止する．
+int
+bddmgr_disable_gc(
+  lua_State* L
+)
+{
+  Luapp lua{L};
+
+  SizeType n = lua.get_top();
+  if ( n != 1 ) {
+    return lua.error_end("Error: BddMgr:disable_gc() expects no arguments.");
+  }
+
+  auto obj = LuaBdd::to_bddmgr(L, 1);
+  ASSERT_COND( obj != nullptr );
+
+  obj->disable_gc();
+
+  lua.push_boolean(true);
+  return 1;
+}
+
+// 変数によるコファクターを求める．
+int
+bdd_vcofactor(
+  lua_State* L
+)
+{
+  Luapp lua{L};
+
+  SizeType n = lua.get_top();
+  if ( n != 2 && n != 3 ) {
+    return lua.error_end("Error: Bdd:is_valid() expects one or two arguments.");
+  }
+
+  auto obj = LuaBdd::to_bdd(L, 1);
+  ASSERT_COND( obj != nullptr );
+
+  bool ok;
+  lua_Integer v;
+  tie(ok, v) = lua.to_integer(2);
+  if ( !ok ) {
+    return lua.error_end("Error: Bdd:vcofactor(): 1st argument should be an integer.");
+  }
+
+  bool inv{false};
+  if ( n == 3 ) {
+    inv = lua.to_boolean(3);
+  }
+
+  auto bddp = bdd_new(lua);
+  *bddp = obj->cofactor(VarId(v), inv);
+
+  return 1;
+}
+
+// 適正な値を持っている時 true を返す．
+int
+bdd_is_valid(
+  lua_State* L
+)
+{
+  Luapp lua{L};
+
+  SizeType n = lua.get_top();
+  if ( n != 1 ) {
+    return lua.error_end("Error: Bdd:is_valid() expects no arguments.");
+  }
+
+  auto obj = LuaBdd::to_bdd(L, 1);
+  ASSERT_COND( obj != nullptr );
+
+  lua.push_boolean(obj->is_valid());
+  return 1;
+}
+
+// 不正値を持っている時 true を返す．
+int
+bdd_is_invalid(
+  lua_State* L
+)
+{
+  Luapp lua{L};
+
+  SizeType n = lua.get_top();
+  if ( n != 1 ) {
+    return lua.error_end("Error: Bdd:is_invalid() expects no arguments.");
+  }
+
+  auto obj = LuaBdd::to_bdd(L, 1);
+  ASSERT_COND( obj != nullptr );
+
+  lua.push_boolean(obj->is_invalid());
+  return 1;
+}
+
+// 恒偽関数の時 true を返す．
+int
+bdd_is_zero(
+  lua_State* L
+)
+{
+  Luapp lua{L};
+
+  SizeType n = lua.get_top();
+  if ( n != 1 ) {
+    return lua.error_end("Error: Bdd:is_zero() expects no arguments.");
+  }
+
+  auto obj = LuaBdd::to_bdd(L, 1);
+  ASSERT_COND( obj != nullptr );
+
+  lua.push_boolean(obj->is_zero());
+  return 1;
+}
+
+// 恒真関数の時 true を返す．
+int
+bdd_is_one(
+  lua_State* L
+)
+{
+  Luapp lua{L};
+
+  SizeType n = lua.get_top();
+  if ( n != 1 ) {
+    return lua.error_end("Error: Bdd:is_one() expects no arguments.");
+  }
+
+  auto obj = LuaBdd::to_bdd(L, 1);
+  ASSERT_COND( obj != nullptr );
+
+  lua.push_boolean(obj->is_one());
+  return 1;
+}
+
+// 積項の時 true を返す．
+int
+bdd_is_cube(
+  lua_State* L
+)
+{
+  Luapp lua{L};
+
+  SizeType n = lua.get_top();
+  if ( n != 1 ) {
+    return lua.error_end("Error: Bdd:is_cube() expects no arguments.");
+  }
+
+  auto obj = LuaBdd::to_bdd(L, 1);
+  ASSERT_COND( obj != nullptr );
+
+  lua.push_boolean(obj->is_cube());
+  return 1;
+}
+
+// 正リテラルの積項の時 true を返す．
+int
+bdd_is_posicube(
+  lua_State* L
+)
+{
+  Luapp lua{L};
+
+  SizeType n = lua.get_top();
+  if ( n != 1 ) {
+    return lua.error_end("Error: Bdd:is_posicube() expects no arguments.");
+  }
+
+  auto obj = LuaBdd::to_bdd(L, 1);
+  ASSERT_COND( obj != nullptr );
+
+  lua.push_boolean(obj->is_posicube());
+  return 1;
+}
+
+// 与えられた変数がサポートの時 true を返す．
+int
+bdd_check_sup(
+  lua_State* L
+)
+{
+  Luapp lua{L};
+
+  SizeType n = lua.get_top();
+  if ( n != 2 ) {
+    return lua.error_end("Error: Bdd:check_sup() expects one argument.");
+  }
+
+  auto obj = LuaBdd::to_bdd(L, 1);
+  ASSERT_COND( obj != nullptr );
+
+  bool ok;
+  lua_Integer v;
+  tie(ok, v) = lua.to_integer(2);
+  if ( !ok ) {
+    return lua.error_end("Error: Bdd:check_sup(): 1st argument should be an integer.");
+  }
+
+  bool b = obj->check_sup(VarId(v));
+  lua.push_boolean(b);
+  return 1;
+}
+
+// 与えられた変数が対称の時 true を返す．
+int
+bdd_check_sym(
+  lua_State* L
+)
+{
+  Luapp lua{L};
+
+  SizeType n = lua.get_top();
+  if ( n != 3 && n != 4) {
+    return lua.error_end("Error: Bdd:check_sup() expects two or three arguments.");
+  }
+
+  auto obj = LuaBdd::to_bdd(L, 1);
+  ASSERT_COND( obj != nullptr );
+
+  bool ok;
+  lua_Integer v1;
+  tie(ok, v1) = lua.to_integer(2);
+  if ( !ok ) {
+    return lua.error_end("Error: Bdd:check_sup(): 1st argument should be an integer.");
+  }
+
+  lua_Integer v2;
+  tie(ok, v2) = lua.to_integer(3);
+  if ( !ok ) {
+    return lua.error_end("Error: Bdd:check_sup(): 2nd argument should be an integer.");
+  }
+
+  bool inv{false};
+  if ( n == 4 ) {
+    inv = lua.to_boolean(4);
+  }
+
+  bool b = obj->check_sym(VarId(v1), VarId(v2), inv);
+  lua.push_boolean(b);
+  return 1;
+}
+
+// サポートを求める．
+int
+bdd_get_support(
+  lua_State* L
+)
+{
+  Luapp lua{L};
+
+  SizeType n = lua.get_top();
+  if ( n != 1 ) {
+    return lua.error_end("Error: Bdd:get_support() expects no arguments.");
+  }
+
+  auto obj = LuaBdd::to_bdd(L, 1);
+  ASSERT_COND( obj != nullptr );
+
+  auto bddp = bdd_new(lua);
+  *bddp = obj->get_support();
+
+  return 1;
+}
+
+// 1となるパスを求める．
+int
+bdd_get_onepath(
+  lua_State* L
+)
+{
+  Luapp lua{L};
+
+  SizeType n = lua.get_top();
+  if ( n != 1 ) {
+    return lua.error_end("Error: Bdd:get_onepath() expects no arguments.");
+  }
+
+  auto obj = LuaBdd::to_bdd(L, 1);
+  ASSERT_COND( obj != nullptr );
+
+  auto bddp = bdd_new(lua);
+  *bddp = obj->get_onepath();
+
+  return 1;
+}
+
+// 0となるパスを求める．
+int
+bdd_get_zeropath(
+  lua_State* L
+)
+{
+  Luapp lua{L};
+
+  SizeType n = lua.get_top();
+  if ( n != 1 ) {
+    return lua.error_end("Error: Bdd:get_zeropath() expects no arguments.");
+  }
+
+  auto obj = LuaBdd::to_bdd(L, 1);
+  ASSERT_COND( obj != nullptr );
+
+  auto bddp = bdd_new(lua);
+  *bddp = obj->get_zeropath();
+
+  return 1;
+}
+
+// 根の変数で分解する．
+int
+bdd_root_decomp(
+  lua_State* L
+)
+{
+  Luapp lua{L};
+
+  SizeType n = lua.get_top();
+  if ( n != 1 ) {
+    return lua.error_end("Error: Bdd:root_decomp() expects no arguments.");
+  }
+
+  auto obj = LuaBdd::to_bdd(L, 1);
+  ASSERT_COND( obj != nullptr );
+
+  Bdd f0;
+  Bdd f1;
+  auto var = obj->root_decomp(f0, f1);
+
+  auto bddp0 = bdd_new(lua);
+  *bddp0 = f0;
+
+  auto bddp1 = bdd_new(lua);
+  *bddp1 = f1;
+
+  lua.push_integer(var.val());
+
+  return 3;
+}
+
+// 根の変数を求める．
+int
+bdd_root_var(
+  lua_State* L
+)
+{
+  Luapp lua{L};
+
+  SizeType n = lua.get_top();
+  if ( n != 1 ) {
+    return lua.error_end("Error: Bdd:root_var() expects no arguments.");
+  }
+
+  auto obj = LuaBdd::to_bdd(L, 1);
+  ASSERT_COND( obj != nullptr );
+
+  auto var = obj->root_var();
+
+  lua.push_integer(var.val());
+  return 1;
+}
+
+// 負のコファクターを求める．
+int
+bdd_root_cofactor0(
+  lua_State* L
+)
+{
+  Luapp lua{L};
+
+  SizeType n = lua.get_top();
+  if ( n != 1 ) {
+    return lua.error_end("Error: Bdd:root_cofactor0() expects no arguments.");
+  }
+
+  auto obj = LuaBdd::to_bdd(L, 1);
+  ASSERT_COND( obj != nullptr );
+
+  auto bddp = bdd_new(lua);
+  *bddp = obj->root_cofactor0();
+
+  return 1;
+}
+
+// 正のコファクターを求める．
+int
+bdd_root_cofactor1(
+  lua_State* L
+)
+{
+  Luapp lua{L};
+
+  SizeType n = lua.get_top();
+  if ( n != 1 ) {
+    return lua.error_end("Error: Bdd:root_cofactor1() expects no arguments.");
+  }
+
+  auto obj = LuaBdd::to_bdd(L, 1);
+  ASSERT_COND( obj != nullptr );
+
+  auto bddp = bdd_new(lua);
+  *bddp = obj->root_cofactor1();
+
+  return 1;
+}
+
+// 根の枝が反転している時に true を返す．
+int
+bdd_root_inv(
+  lua_State* L
+)
+{
+  Luapp lua{L};
+
+  SizeType n = lua.get_top();
+  if ( n != 1 ) {
+    return lua.error_end("Error: Bdd:root_inv() expects no arguments.");
+  }
+
+  auto obj = LuaBdd::to_bdd(L, 1);
+  ASSERT_COND( obj != nullptr );
+
+  bool b = obj->root_inv();
+  lua.push_boolean(b);
+
+  return 1;
+}
+
+// 評価を行う．
+int
+bdd_eval(
+  lua_State* L
+)
+{
+  Luapp lua{L};
+
+  SizeType n = lua.get_top();
+  if ( n != 2 ) {
+    return lua.error_end("Error: Bdd:eval() expects one argument.");
+  }
+
+  auto obj = LuaBdd::to_bdd(L, 1);
+  ASSERT_COND( obj != nullptr );
+
+  bool b = obj->eval(inputs);
+  lua.push_boolean(b);
+
+  return 1;
+}
+
+// ノード数を返す．
+int
+bdd_size(
+  lua_State* L
+)
+{
+  Luapp lua{L};
+
+  SizeType n = lua.get_top();
+  if ( n != 1 ) {
+    return lua.error_end("Error: Bdd:size() expects no arguments.");
+  }
+
+  auto obj = LuaBdd::to_bdd(L, 1);
+  ASSERT_COND( obj != nullptr );
+
+  lua.push_integer(obj->size());
+
+  return 1;
+}
+
+// 変数のリストに変換する．
+int
+bdd_to_varlist(
+  lua_State* L
+)
+{
+  Luapp lua{L};
+
+  SizeType n = lua.get_top();
+  if ( n != 1 ) {
+    return lua.error_end("Error: Bdd:to_varlist() expects no arguments.");
+  }
+
+  auto obj = LuaBdd::to_bdd(L, 1);
+  ASSERT_COND( obj != nullptr );
+
+  auto varlist = obj->to_varlist();
+
+  return 1;
+}
+
+// 真理値表の文字列に変換する．
+int
+bdd_to_truth(
+  lua_State* L
+)
+{
+  Luapp lua{L};
+
+  SizeType n = lua.get_top();
+  if ( n != 2 ) {
+    return lua.error_end("Error: Bdd:to_truth() expects one argument.");
+  }
+
+  auto obj = LuaBdd::to_bdd(L, 1);
+  ASSERT_COND( obj != nullptr );
+
+  bool ok;
+  lua_Integer v;
+  tie(ok, v) = lua.to_integer(2);
+  if ( !ok ) {
+    return lua.error_end("Error: Bdd:to_truth(): 1st argument should be an integer.");
+  }
+
+  auto str = obj->to_truth(v);
+  lua.push_string(str.c_str());
 
   return 1;
 }
@@ -287,7 +903,38 @@ bdd_display(
     obj->display(s);
   }
 
-  return 0;
+  lua.push_boolean(true);
+  return 1;
+}
+
+// dot 形式で出力する．
+int
+bdd_gen_dot(
+  lua_State* L
+)
+{
+  Luapp lua{L};
+
+  SizeType n = lua.get_top();
+  if ( n != 2 ) {
+    return lua.error_end("Error: Bdd:gen_dot() expects one argument.");
+  }
+
+  auto obj = LuaBdd::to_bdd(L, 1);
+  ASSERT_COND( obj != nullptr );
+
+  auto filename = lua.to_string(2);
+  ofstream s{filename};
+  if ( !s ) {
+    ostringstream buf;
+    buf << "Error: Bdd:gen_dot(): Could not create "
+	<< filename << ".";
+    return lua.error_end(buf.str());
+  }
+  obj->gen_dot(s);
+
+  lua.push_boolean(true);
+  return 1;
 }
 
 END_NONAMESPACE
@@ -309,15 +956,44 @@ LuaBdd::init(
     {"posi_literal", bddmgr_posi_literal},
     {"nega_literal", bddmgr_nega_literal},
     {"from_truth",   bddmgr_from_truth},
-    {nullptr,         nullptr}
+    {"node_num",     bddmgr_node_num},
+    {"gc_limit",     bddmgr_gc_limit},
+    {"set_gc_limit", bddmgr_set_gc_limit},
+    {"enable_gc",    bddmgr_enable_gc},
+    {"disable_gc",   bddmgr_disable_gc},
+    {nullptr,        nullptr}
   };
 
   lua.reg_metatable(BDDMGR_SIGNATURE, bddmgr_gc, bddmgr_mt);
 
   // Bdd のメンバ関数(メソッド)テーブル
   static const struct luaL_Reg bdd_mt[] = {
-    {"display",       bdd_display},
-    {nullptr,         nullptr}
+    {"cofactor",       bdd_cofactor},
+    {"vcofactor",      bdd_vcofactor},
+    {"is_valid",       bdd_is_valid},
+    {"is_invalid",     bdd_is_invalid},
+    {"is_zero",        bdd_is_zero},
+    {"is_one",         bdd_is_one},
+    {"is_cube",        bdd_is_cube},
+    {"is_posicube",    bdd_is_posicube},
+    {"check_sup",      bdd_check_sup},
+    {"check_sym",      bdd_check_sym},
+    {"get_suport",     bdd_get_support},
+    {"get_onepath",    bdd_get_onepath},
+    {"get_zeropath",   bdd_get_zeropath},
+    {"root_decomp",    bdd_root_decomp},
+    {"root_var",       bdd_root_var},
+    {"root_cofactor0", bdd_root_cofactor0},
+    {"root_cofactor1", bdd_root_cofactor1},
+    {"root_inv",       bdd_root_inv},
+    {"eval",           bdd_eval},
+    {"size",           bdd_size},
+    {"to_varlist",     bdd_to_varlist},
+    {"to_litlist",     bdd_to_litlist},
+    {"to_truth",       bdd_to_truth},
+    {"display",        bdd_display},
+    {"gen_dot",        bdd_gen_dot},
+    {nullptr,          nullptr}
   };
 
   lua.reg_metatable(BDD_SIGNATURE, bdd_gc, bdd_mt);
